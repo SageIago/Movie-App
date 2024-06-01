aef9d7824b72e6c26aa1adc82a296cc285869faa
Sage_Iago <158774357+SageIago@users.noreply.github.com>
2024-06-01 23:08:42 +0100

    Building a Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
   return (
-    <>
-
-    </>
+    <Grid templateAreas= {{
+      base: `"nav" "main"`, // On Mobile Devices
+      lg: `"nav nav" "aside main"` // On Large Devices
+    }}>
+      <GridItem area="nav">Nav</GridItem>
+      <Show above="lg">
+      <GridItem area="nav">Aside</GridItem>
+      </Show>
+      <GridItem area="nav">Main</GridItem>
+    </Grid>
   )
 }
 
